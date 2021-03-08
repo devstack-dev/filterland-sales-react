07685a8988d5ff793321992b1f9d5a7e7d37b844
ukaemma2 <ukaemmanuel200@gmail.com>
2021-03-08 19:59:03 +0100

    fixed bugs

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -18,43 +18,43 @@ export default function Footer(props) {
                             <Col md="8" sm="12 " className="foote">
                                 <div className="footer-de">Let's keep in touch</div>
                                 <a href="#!"><img className="images_sets" src={Images1} alt="filterland" /></a>
-                                <a href="#!"> <span><img className="images_sets" src={Images2} alt="filterland" /></span></a>
+                                <a href="#!" className="ac-h"> <span><img className="images_sets" src={Images2} alt="filterland" /></span></a>
                                 <a href="#!"><span><img className="images_sets" src={Images3} alt="filterland" /></span></a>
 
                             </Col>
                             <Col md="2" sm="12" className="ces">
                                 <div className="footer-des">Useful Links</div>
-                                <ul>
-                                    <li className="list-unstyled">
+                                <div>
+                                    <div className="list-unstyled">
                                         <a href="#!">About Us</a>
-                                    </li>
-                                    <li className="list-unstyled">
+                                    </div>
+                                    <div className="list-unstyled">
                                         <a href="#!">Blog</a>
-                                    </li>
-                                    <li className="list-unstyled">
+                                    </div>
+                                    <div className="list-unstyled">
                                         <a href="#!">Use Cases</a>
-                                    </li>
-                                    <li className="list-unstyled">
+                                    </div>
+                                    <div className="list-unstyled">
                                         <a href="#!">Contact Us</a>
-                                    </li>
-                                </ul>
+                                    </div>
+                                </div>
                             </Col>
                             <Col md="2" className="ces">
                                 <div className="footer-des">Resources</div>
-                                <ul>
-                                    <li className="list-unstyled">
+                                <div>
+                                    <div className="list-unstyled">
                                         <a href="#!">About Us</a>
-                                    </li>
-                                    <li className="list-unstyled">
+                                    </div>
+                                    <div className="list-unstyled">
                                         <a href="#!">Blog</a>
-                                    </li>
-                                    <li className="list-unstyled">
+                                    </div>
+                                    <div className="list-unstyled">
                                         <a href="#!">Use Cases</a>
-                                    </li>
-                                    <li className="list-unstyled">
+                                    </div>
+                                    <div className="list-unstyled">
                                         <a href="#!">Contact Us</a>
-                                    </li>
-                                </ul>
+                                    </div>
+                                </div>
                             </Col>
                         </Row>
                     </Container>
